dec8c477900363532c0251618e7e2ec4ec44bda0
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-07-24 22:56:47 +0530

    ok

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import { Search, BookText, Clock5, Handshake } from 'lucide-react';
+import ActivityTable from '../components/Dashboard/ActivityTable';
 
 const Dashboard = () => {
   return (
     <>
-      <div className="w-full flex flex-row items-start">
-        <Sidebar />
+      {/* <div className="w-full flex flex-row items-start">
+        <Sidebar /> */}
         <div className="w-full flex flex-col gap-4 px-4 py-4 items-center">
-          <Navbar />
           <div className="w-full px-4 flex py-5 gap-5">
             <div className="w-1/3 flex bg-gradient-to-r gap-2 from-violet-200 to-pink-200 rounded-3xl text-violet-950 items-start px-8 flex-col py-10 ">
-              <h1 className="text-3xl font-bold">Hello Sreeram 👋</h1>
+              <h1 className="text-3xl font-bold">Hello Sreeram 👋!</h1>
               <p className="text-lg popp">Welcome to the dashboard</p>
             </div>
             <div className="w-2/3 stats shadow border">
@@ -49,8 +49,9 @@ const Dashboard = () => {
 
             </div>
           </div>
+          <ActivityTable />
         </div>
-      </div>
+      {/* </div> */}
     </>
   )
 }
---
+++ client/src/pages/ReturnBooks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReturnBooks = () => {
+  return (
+    <div>ReturnBooks</div>
+  )
+}
+
+export default ReturnBooks
