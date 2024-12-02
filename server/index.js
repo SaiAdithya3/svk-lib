98f83c6f3f58b6201c16dceeef5299950f82d97f
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-12-02 11:50:43 +0530

    changed origin

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  // origin: 'http://localhost:5173',
+  origin: 'https://sku-lms.vercel.app/',
   credentials: true
 }));
 app.use(bodyParser.json());
